aa00804a1edc48d5dafd2762f39022720a8201f8
sunndayolvera-lab <sunnday.olvera@gmail.com>
2026-01-02 21:56:36 +0100

    Delete pack_01 directory

--- pack_01/sunn_double_barrel_stand/fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'cerulean'
-game 'gta5'
-lua54 'yes'
-this_is_a_map 'yes'
-creator 'Sunn-Day'
-
-data_file 'DLC_ITYP_REQUEST' 'stream/sunn_double_barrel_stand.ytyp'
-
-dependency '/assetpacks'

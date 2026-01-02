57980bb0c04ac9e915868a38a88d99c40fb7b1db
sunndayolvera-lab <sunnday.olvera@gmail.com>
2026-01-02 21:42:07 +0100

    Add files via upload

---
+++ pack_01/all_props/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+this_is_a_map 'yes'
+creator 'Sunn-Day'
+
+-- UN SEUL fichier ytyp pour toutes les props !
+data_file 'DLC_ITYP_REQUEST' 'stream/sunn_props_pack.ytyp'
+
+dependency '/assetpacks'
